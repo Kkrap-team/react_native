9b758f63f1b29c2f600200c73622fabb6469a935
ghks8525 <ghks8525@gamil.com>
2025-03-06 22:47:00 +0900

    feat : TextStyle 모듈화
    convention : 폰트+글자크기_ 색상_글자타입

---
+++ src/theme/TextTheme.tsx
@@ -0,0 +1,61 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    noto14_b3b3b3: {
+        fontSize: 14,
+        fontFamily: 'NotoSansKR-Regular',
+        color: '#B3B3B3',
+    },
+    noto14_b3b3b3_Bold: {
+        fontSize: 14,
+        fontFamily: 'NotoSansKR-Bold',
+        color: '#B3B3B3',
+    },
+    noto16_b3b3b3: {
+        fontSize: 16,
+        fontFamily: 'NotoSansKR-Regular',
+        color: '#B3B3B3',
+    },
+    noto16_b3b3b3_Bold: {
+        fontSize: 16,
+        fontFamily: 'NotoSansKR-Bold',
+        color: '#B3B3B3',
+    },
+    noto16_000000: {
+        fontSize: 16,
+        fontFamily: 'NotoSansKR-Regular',
+        color: '#000000',
+    },
+    noto16_000000_Bold: {
+        fontSize: 16,
+        fontFamily: 'NotoSansKR-Bold',
+        color: '#000000',
+    },
+    noto28_000000: {
+        fontSize: 28,
+        fontFamily: 'NotoSansKR-Regular',
+        color: '#000000',
+        lineHeight: 32,
+    },
+    noto28_000000_Bold: {
+        fontSize: 28,
+        fontFamily: 'NotoSansKR-Bold',
+        color: '#000000',
+        lineHeight: 32,
+
+    },
+    noto28_5AB7FC: {
+        fontSize: 28,
+        fontFamily: 'NotoSansKR-Regular',
+        color: '#5AB7FC',
+        lineHeight: 32,
+    },
+    noto28_5AB7FC_Bold: {
+        fontSize: 28,
+        fontFamily: 'NotoSansKR-Bold',
+        color: '#5AB7FC',
+        lineHeight: 32,
+    },
+});
+
+export default styles;
